1f65818323646c57ee939b0feac9cd162899a4aa
douglasnotfunny <contatodouglassiqueira@gmail.com>
2021-10-04 18:17:17 -0300

    starting api

---
+++ crud_playlist/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from rest_framework import routers
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+]
